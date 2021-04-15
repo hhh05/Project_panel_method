8435659e824555e94303efb2c0c70bff63373af0
Henri <henri.parez@student.uclouvain.be>
2021-04-15 10:29:28 +0200

    starting commit

---
+++ panel_method.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 15 10:27:28 2021
+
+@author: henri and julien
+"""
+""" The goal of this code is to compute the velocity around an airfoil with the linear vortex sheet
+panel method"""
