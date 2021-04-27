910d4b5cfa6d48f0ff65ea3fdb86307f61203cbb
Henri <henri.parez@student.uclouvain.be>
2021-04-27 10:07:29 +0200

    Adding the right hand side of the equation, code able to obtains the gamma's

--- panel_method.py
+++ panel_method.py
@@ -38,7 +38,9 @@ class Panel:
             
         self.nx = np.cos(self.angle)
         self.ny = np.sin(self.angle)
+        self.normal = np.array([self.nx,self.ny])
         
+        self.longitudinal = np.array([self.xf-self.xi,self.yf-self.yi])
         
 def discretization(n):
     #(n-1)*2 panels
@@ -54,6 +56,9 @@ def discretization(n):
     x=np.append(x1,x2)
     y=np.append(y1,y2)
     
+#    print(x)
+#    print(y)
+    
     print("size x",np.size(x))
     
     print("size y",np.size(y))
@@ -64,14 +69,20 @@ def discretization(n):
     
     panels = np.empty((n-1)*2, dtype=object)
     for i in range(0, np.size(x)-1):
+#        print("yi",y[i],i)
+#        print("yi+1",y[i+1],i+1)
         panels[i] = Panel(x[i],x[i+1],y[i],y[i+1])
     N=2*(n-1)
     return N,x,y,panels
     
 N,x,y,panels = discretization(20)
 
-gamma=np.zeros(len(x)) #gamma[0] must be equal to -gamma[-1]
-A=np.zeros((len(x),len(x)))
+
+"""Creation of left and side of the equation"""
+
+
+gamma=np.zeros(N+1) #gamma[0] must be equal to -gamma[-1]
+A=np.zeros((N+1,N+1))
 
 
 def u(x,y,a,b):
@@ -84,11 +95,11 @@ def contribution(x,y,b):
     A=(-1/(2*np.pi))*(y/2)*np.log(((x-b)**2 + y**2)/((x+b)**2 + y**2))
     B=x*( (np.arctan((x-b)/y)) - (np.arctan((x+b)/y)) )*(-1/(2*np.pi))
     C=( (np.arctan((x-b)/y)) - (np.arctan((x+b)/y)) )*(-1/(2*np.pi))
-    contribu = [(A-B-C)/(2*b) , (B-A-C)/(2*b)]
+    contribu = np.array([(A-B-C)/(2*b) , (B-A-C)/(2*b)])
     
     F= (-1/(2*np.pi))*(-2*x + y*( np.arctan((x-b)/y) - np.arctan((x+b)/y) ) )
     G= (-1/(2*np.pi))*np.log(((x-b)**2 + y**2)/((x+b)**2 + y**2))
-    contribv = [(F+(G/2)*(x+1) )/2*b, ((G/2)*(1-x) - F)/2*b]
+    contribv = np.array([(F+(G/2)*(x+1) )/2*b, ((G/2)*(1-x) - F)/2*b])
     
     return contribu, contribv
 
@@ -96,32 +107,63 @@ for i in range(0,len(panels)):
     panel_receiver=panels[i]
     for j in range(0,len(panels)):
         panel_sender=panels[j]
-        a=np.array([panel_receiver.xc-panel_sender.xc,panel_receiver.yc-panel_sender.yc])
-        b=np.array([panel_sender.xf-panel_sender.xi,panel_sender.yf-panel_sender.yi])
-        cos = np.dot(a,b)/ ( np.linalg.norm(a)*np.linalg.norm(b) )
-        sin = np.sqrt(1-cos**2) 
-        x= np.linalg.norm(a)*cos
-        y= np.linalg.norm(a)*sin 
-        contribu, contribv = contribution(x,y,panel_sender.length)
-        #print(contribu[0]+contribv[0])
-        #print(contribu[1]+contribv[1])
-        #####Rajouter le produit scalaire entre u,v et la normale au panel receiver !!!!!!!!!!!!!!!!
-        A[i,j]+= contribu[0]+contribv[0]
-        A[i,j+1]+= contribu[1]+contribv[1]
-        #print("a",a,"b",b)
-        
+        if (i!=j):
+            a=np.array([panel_receiver.xc-panel_sender.xc,panel_receiver.yc-panel_sender.yc])
+#            print("a",a,"j",j)
+            b=np.array([panel_sender.xf-panel_sender.xi,panel_sender.yf-panel_sender.yi])
+#            print("a norm",np.linalg.norm(a),"j:",j)
+#            print("panel_receiver.xc",panel_receiver.xc)
+#            print("panel_receiver.yc",panel_receiver.yc)
+#            print("panel_sender.xc",panel_sender.xc)
+#            print("panel_receiver.yc",panel_sender.yc)
+            cos = np.dot(a,b)/ ( np.linalg.norm(a)*np.linalg.norm(b) )
+            sin = np.sqrt(1-cos**2) 
+            xp= np.linalg.norm(a)*cos
+            yp= np.linalg.norm(a)*sin 
+            contribu, contribv = contribution(xp,yp,panel_sender.length/2)
+            contribu = contribu * np.dot(panel_receiver.normal, panel_sender.longitudinal)
+            contribu = contribu * np.dot(panel_receiver.normal,panel_sender.normal)
+            #print(contribu[0]+contribv[0])
+            #print(contribu[1]+contribv[1])
+            A[i,j]+= contribu[0]+contribv[0]
+            A[i,j+1]+= contribu[1]+contribv[1] 
+            #print("a",a,"b",b)
+ 
+#Equation to have gamma0=-gamma(N+1)       
+A[-1,0]=1 
+A[-1,-1]=-1
+
+
+
+"""Creation of right hand side of the equation"""
+Uinf=30*np.array([1,0])
+bright=np.zeros(N+1)
+
+for k in range(0,len(panels)):
+    panel=panels[k]
+    bright[k]=-np.dot(Uinf,panel.normal)
+    
+"""Solving the system"""
+gamma_solve = np.linalg.solve(A,bright)
+print(gamma_solve)
+    
+
+
+
 
 
 
 """Print for debugging"""
 
-
-
-
 ####print for debugging###
 #for i in range(0,np.size(panels)): 
 #    print("Panel",i+1)
 #    print("xi",panels[i].xi)
+#    print("xf",panels[i].xf)
+#    print("yi",panels[i].yi)
+#    print("yf",panels[i].yf)
+#    print("xc",panels[i].xc)
+#    print("yc",panels[i].yc)
 #    print(panels[i].length)
 #    print(np.rad2deg(panels[i].angle))
 #    print("nx",panels[i].nx)
